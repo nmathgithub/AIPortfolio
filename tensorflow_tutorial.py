11d69ee6a79b5736892147e0a3213e788f559594
Nitesh Mathur <nim081@utulsa.edu>
2022-09-05 15:32:33 -0500

    Sequencing Tutorial

--- tensorflow_tutorial.py
+++ tensorflow_tutorial.py
@@ -1,18 +1,39 @@
+from msilib import datasizemask
 import tensorflow as tf 
 from tensorflow import keras
 from tensorflow.keras.preprocessing.text import Tokenizer
+from tensorflow.keras.preprocessing.sequence import pad_sequences
 
 # Tutorial (YouTube)
 # Natural Language Process - Tokenization (NLP Zero to Hero - Part I)
 # 5 September 2022 
 
+# Training Data (basic example)
 sentences = [
     'I love my dog',
     'I love my cat',
-    'You love my dog!'
+    'You love my dog!', 
+    'Do you think my dog is amazing?'
 ]
 
-tokenizer = Tokenizer(num_words = 100)
+
+
+tokenizer = Tokenizer(num_words = 100, oov_token = "<OOV>")
 tokenizer.fit_on_texts(sentences)
 word_index = tokenizer.word_index 
+
+sequences = tokenizer.texts_to_sequences(sentences)
+
+padded = pad_sequences(sequences, padding = 'post', 
+                       truncating = 'post', maxlen=5)
 print(word_index)
+print(sequences)
+print(padded)
+# Test Data 
+test_data = [
+    'I really love my dog', 
+    'my dog loves my manatee'
+]
+
+test_seq = tokenizer.texts_to_sequences(test_data)
+print(test_seq)
