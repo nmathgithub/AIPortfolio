f91eff8afed430b80571afdf0c7727a0eea53832
Nitesh Mathur <nim081@utulsa.edu>
2022-09-05 16:14:58 -0500

    Sarcasm Project Files Added

---
+++ sarcasm_NN_tutorial.py
@@ -0,0 +1,14 @@
+import json 
+
+with open("sarcasm2.json", 'r') as f:
+    datastore = json.load(f)
+
+sentences = []
+labels = []
+urls = []
+for item in datastore:
+    sentences.append(item['headline'])
+    labels.append(item['is_sarcastic'])
+    urls.append(item['article_link'])
+
+print(sentences)
