8b1ffb1fdeac05ca1c5ca615409f4d162028373d
Nitesh Mathur <nim081@utulsa.edu>
2022-09-21 17:24:40 -0500

    Download Flower Data Set

---
+++ image_classify.py
@@ -0,0 +1,13 @@
+import matplotlib.pyplot as plt
+import numpy as np 
+import PIL 
+import tensorflow as tf 
+
+from tensorflow import keras
+from tensorflow.keras import layers 
+from tensorflow.keras.models import Sequential 
+
+import pathlib 
+dataset_url = "https://storage.googleapis.com/download.tensorflow.org/example_images/flower_photos.tgz"
+data_dir = tf.keras.utils.get_file('flower_photos', origin=dataset_url, untar=True)
+data_dir = pathlib.Path(data_dir)
